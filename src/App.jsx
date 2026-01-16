d6e896eeba9a8b2d55fa2ec4590e4c429af1d367
Pavithra-45 <pavithratv2019@gmail.com>
2026-01-16 13:42:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./components/Header"
+import AddContact from "./components/AddContact"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <AddContact/>
+    </div>
+  )
+}
+
+export default App
