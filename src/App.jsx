8b88f425852ceee895ec39b66bee4bfcf7bc41ff
Pavithra-45 <pavithratv2019@gmail.com>
2026-01-17 19:23:57 +0530

    Add contact manager features and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {v4 as uuid} from "uuid";
 import Header from "./components/Header"
 import AddContact from "./components/AddContact"
+import ContactList from './components/ContactList'
+
+const LOCAL_STORAGE_KEY = "contacts";
 
 function App() {
+const [contacts, setContacts] = useState(() => {
+  const stored = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return stored ? JSON.parse(stored) : [];
+});
+
+
+const addContactHandler = (contact)=>{
+  console.log(contact);
+  setContacts([...contacts,{ id:uuid(),...contact}])
+}
+
+const removeContactHandler = (id)=>{
+  const newContactList = contacts.filter((contact)=>{
+    return contact.id !== id;
+  });
+  setContacts(newContactList);
+}
+
+useEffect(()=>{
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+}, [contacts]);
+
+useEffect(()=>{
+  const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if(retrievedContacts) setContacts(retrievedContacts);
+}, []);
+
   return (
-    <div>
+    <div className='ui container'>
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   )
 }
---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import user from '../images/user1.png'
+
+
+function ContactCard(props) {
+    const {id, name, email} = props.contact
+  return (
+    <div className='item'>
+        <img className='ui avatar image' src={user} alt="" />
+                <div className='content'>
+                    <div className='header'>{name}</div>
+                    <div>{email}</div>
+                    <i className='trash alternate outline icon right floated'
+                    style={{color:"red", marginTop:"7px"}}
+                    onClick={()=>props.clickHandler(id)}
+                    ></i>
+                </div>
+            </div>
+  )
+}
+
+export default ContactCard
